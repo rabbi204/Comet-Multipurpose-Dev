743b9b6d98f50593805424d700b374f777fa7de6
rabbi204 <md.rabbi.web@gmail.com>
2021-04-25 12:14:26 +0600

    Mail or Notifiaction Problem

---
+++ app/Http/Controllers/Auth/EmailConfirmationMail.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+
+class EmailConfirmationMail
+{
+
+    /**
+     * EmailConfirmationMail constructor.
+     * @param array $mail_details
+     */
+    public function __construct(array $mail_details)
+    {
+    }
+}
